f0065430de64bb09dda078c1576fc24f02e57b4b
Bridgette Campbell <bridgette.campbellbookwyrm@gmail.com>
2017-10-20 11:31:09 -0700

    Initial commit

---
+++ src/main/Message.java
@@ -0,0 +1,12 @@
+package main;
+
+/**
+ * The message object. Holds the message contents and writer display name.
+ * 
+ * @author Bridgette Campbell
+ * 
+ */
+
+public class Message {
+	
+}
---
+++ src/main/MessageChannel.java
@@ -0,0 +1,15 @@
+package main;
+
+
+
+/**
+ * The message channel. 
+
+ * 
+ * @author Bridgette Campbell
+ *
+ */
+
+public class MessageChannel {
+	
+}
