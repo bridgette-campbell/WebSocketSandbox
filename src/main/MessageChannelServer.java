ac4c56b46ad4f472f4939e46f5eb7c3c07340dc8
Bridgette Campbell <bridgette.campbellbookwyrm@gmail.com>
2017-10-20 14:08:58 -0700

    unfinished

--- src/main/MessageChannelServer.java
+++ src/main/MessageChannelServer.java
@@ -1,5 +1,46 @@
 package main;
 
-public class MessageChannelServer {
+import java.io.*;
+import java.net.*;
 
+/**
+ * The message channel server. 
+
+ * 
+ * @author Bridgette Campbell
+ *
+ */
+
+public class MessageChannelServer {
+	
+	private ServerSocket serverSocket;
+	private Socket clientSocket;
+	private PrintWriter out;
+	private BufferedReader in;
+	
+	public void start(int port) throws IOException{
+        serverSocket = new ServerSocket(port);
+        clientSocket = serverSocket.accept();
+        out = new PrintWriter(clientSocket.getOutputStream(), true);
+        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        String greeting = in.readLine();
+            if ("hello server".equals(greeting)) {
+                out.println("hello client");
+            }
+            else {
+                out.println("unrecognised greeting");
+            }
+    }
+ 
+    public void stop() throws IOException {
+        in.close();
+        out.close();
+        clientSocket.close();
+        serverSocket.close();
+    }
+    public static void main(String[] args) throws IOException {
+        MessageChannelServer server=new MessageChannelServer();
+        server.start(6666);
+    }
+	
 }
--- src/main/MessageChannelSocket.java
+++ src/main/MessageChannelSocket.java
@@ -1,5 +1,15 @@
 package main;
 
+import java.io.*;
+import java.net.*;
+
+/**
+ * The message channel socket.
+ * 
+ * @author Bridgette Campbell
+ *
+ */
+
 public class MessageChannelSocket {
 
 }
