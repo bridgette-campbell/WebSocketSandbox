c0cd6e77303bafb1e10d1940a658c4740bb14068
Bridgette Campbell <bridgette.campbellbookwyrm@gmail.com>
2017-10-20 14:25:25 -0700

    unfinished

--- src/main/MessageChannelSocket.java
+++ src/main/MessageChannelSocket.java
@@ -4,7 +4,7 @@ import java.io.*;
 import java.net.*;
 
 /**
- * The message channel socket.
+ * The message channel socket. The client. 
  * 
  * @author Bridgette Campbell
  *
@@ -12,4 +12,26 @@ import java.net.*;
 
 public class MessageChannelSocket {
 
+    private Socket clientSocket;
+    private PrintWriter out;
+    private BufferedReader in;
+ 
+    public void startConnection(String ip, int port) throws UnknownHostException, IOException {
+        clientSocket = new Socket(ip, port);
+        out = new PrintWriter(clientSocket.getOutputStream(), true);
+        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+    }
+ 
+    public String sendMessage(String msg) throws IOException {
+        out.println(msg);
+        String resp = in.readLine();
+        return resp;
+    }
+ 
+    public void stopConnection() throws IOException {
+        in.close();
+        out.close();
+        clientSocket.close();
+    }
+    
 }
